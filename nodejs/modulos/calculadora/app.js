7cb863a09bb5f14cff047745763e66289723832e
Wallan Peres Steffen <wallanperessteffen@gmail.com>
2025-10-03 16:43:22 -0300

    v1.0.1

--- nodejs/modulos/calculadora/app.js
+++ nodejs/modulos/calculadora/app.js
@@ -1,18 +1,15 @@
-import(somar, subtrair, multiplicar, dividir) from './funcoes_matematicas.js';
-import * as calculadoraidade from './calculadora_idade.js';
-
-const calculadoraidade = require('./calculadora_idade');
-const { somar } = require('./funcoes_matematicas');
+import {somar, subtrair, multiplicar, dividir} from './modulos/funcoes_matematicas.js';
+import * as calculadoraidade from './modulos/calculadora_idade.js';
 
 
 
-console.log(`Resultado da soma: ${funcoes_matematica.somar(1, 1)}`);
+console.log(`Resultado da soma: ${somar(1, 1)}`);
 
-console.log(`Resultado da subtração: ${funcoes_matematica.subtrair(8, 4)}`);
+console.log(`Resultado da subtração: ${subtrair(8, 4)}`);
 
-console.log(`Resultado da multiplicação: ${funcoes_matematica.multiplicar(2, 3)}`);
+console.log(`Resultado da multiplicação: ${multiplicar(2, 3)}`);
 
-console.log(`Resultado da divisão: ${funcoes_matematica.dividir(32, 4)}`);
+console.log(`Resultado da divisão: ${dividir(32, 4)}`);
 
 
 
